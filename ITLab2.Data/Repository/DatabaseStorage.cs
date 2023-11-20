1eae41210b24653b3ff076610e9e905a6d8168b3
Nastya <nserytsan@gmail.com>
2023-11-20 18:13:37 +0200

    fix

--- ITLab2.Data/Repository/DatabaseStorage.cs
+++ ITLab2.Data/Repository/DatabaseStorage.cs
@@ -5,7 +5,7 @@ namespace ITLab2.Data.Repository
 {
     public class DatabaseStorage(DbContextOptions<DatabaseStorage> options) : DbContext(options)
     {
-        public DbSet<Database> Databases => Set<Data>();
+        public DbSet<Database> Databases => Set<Database>();
         public DbSet<Table> Tables => Set<Table>();
     }
 }
