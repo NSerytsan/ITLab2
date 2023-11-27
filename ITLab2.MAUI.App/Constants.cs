3e7103cae7d841179b78351b1ca639157e8d12b3
Nastya <nserytsan@gmail.com>
2023-11-28 01:41:29 +0200

    fix

--- ITLab2.MAUI.App/Constants.cs
+++ ITLab2.MAUI.App/Constants.cs
@@ -5,6 +5,6 @@
         public static string LocalhostUrl = DeviceInfo.Platform == DevicePlatform.Android ? "10.0.2.2" : "localhost";
         public static string Scheme = "http";
         public static string Port = "5123";
-        public static string DatabasesRestUrl = $"{Scheme}://{LocalhostUrl}:{Port}/databases/{{0}}";
+        public static string DatabasesRestUrl = $"{Scheme}://{LocalhostUrl}:{Port}/database/{{0}}";
     }
 }
